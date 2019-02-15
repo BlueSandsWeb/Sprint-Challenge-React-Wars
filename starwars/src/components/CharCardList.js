951f637789fbe48388b6b60e68950c1faf5fd820
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-02-15 10:58:06 -0800

    created card component, added some styling with more to go.

--- starwars/src/components/CharCardList.js
+++ starwars/src/components/CharCardList.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import './StarWars.css';
-// import CharCard from './CharCard';
+import CharCard from './CharCard';
 
 function CharCardList(props) {
   return (
     <div className="cardList">{props.starwarsChars.map(char => {
       return (
-        <div key={char.name} className="card">
-          <h3>{char.name}</h3>
-          <div>
-            <ul>
-              <li>Birth Year: {char.birth_year}</li>
-              <li>Gender: {char.gender}</li>
-              <li>Height: {char.height}</li>
-              <li>Mass: {char.mass}</li>
-              <li>Hair Color: {char.hair_color}</li>
-              <li>Eye Color: {char.eye_color}</li>
-            </ul>
-            <img src={char.homeworld} alt=""/>
-          </div>
-        </div>
+        <CharCard key={char.name} char={char}/>
         );}
     )}</div>
   );
